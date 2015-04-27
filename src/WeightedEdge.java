0bdf30e18cfd10a70a4e02200fcb25e4f1a3380d
Chris Campell <ccampell.asu@gmail.com>
2015-04-27 08:49:14 -0400

    Added WeightedEdge Class
    Added the helper class governing edge connections.

---
+++ src/WeightedEdge.java
@@ -0,0 +1,53 @@
+/**
+ * WeightedEdge.java 
+ * A simple utility class for storing and retrieving weighted graph edges. 
+ * @author Chris Campell
+ * @date 4/24/2015
+ * @version 1.0
+ */
+public class WeightedEdge implements Comparable<WeightedEdge>{
+	private int i; 
+	private int j; 
+	private int weight; 
+	
+	/**
+	 * Constructor for Objects of type WeightedEdge. 
+	 * @param start -the first point. 
+	 * @param endpoint -the second point/endpoint. 
+	 * @param weight -the weight of the edge. 
+	 */
+	public WeightedEdge(int start, int endpoint, int weight) {
+		this.i = start; 
+		this.j = endpoint; 
+		this.weight = weight; 
+	}
+	/**
+	 * Accessor method for start point i. 
+	 * @return i -the starting point for the edge. 
+	 */
+	public int getStartPoint() {
+		return i; 
+	}
+	/**
+	 * Accessor method for end point j. 
+	 * @return j -the end point for the edge. 
+	 */
+	public int getEndPoint() {
+		return j; 
+	}
+	/**
+	 * Accessor method for the edge's weight. 
+	 * @return weight -the weight of the edge. 
+	 */
+	public int getWeight() {
+		return weight; 
+	}
+	/**
+	 * Overridden compareTo() compares the weights of two edges.
+	 * @param WeightedEdge -The edge to compare against.  
+	 */
+	@Override
+	public int compareTo(WeightedEdge obj) {
+		return (obj.getWeight() - this.weight); 
+	}
+}
